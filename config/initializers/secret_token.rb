6de0b079c111f8b974ad3847cbd34de13e28e5c3
Yu Fang <taro@taro-System-Product-Name.(none)>
2014-09-22 22:33:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'bc4f3b040a254183f5491d2ac74b5fa712bdc2bfb17cdcb909b110aabe27d0df1ad3c88ec257035aaff0a264d28d9f50605b9a4bb43074639f31d9fc149ba4e9'
